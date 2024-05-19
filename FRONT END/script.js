651918739dc025eb29b6d2a278e12734564bcd04
Bruno Castro Tomaz <bruno.tomaz@mackenzista.com.br>
2024-05-19 14:25:28 -0300

    Adicionado primeira versao do front end, com funcionalidades basicas do site (HTML, CSS e JavaScript)

---
+++ FRONT END/script.js
@@ -0,0 +1,7 @@
+// Botão "Agendar Consulta"
+const botaoAgendar = document.getElementById('agendar-consulta');
+
+botaoAgendar.addEventListener('click', () => {
+    // Redireciona para a página de agendamento
+    window.location.href = 'agendamento.html';
+});
